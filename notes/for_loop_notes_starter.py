81104bce6b7b06422a5351e8fab6e7563ee2ec59
Geoff Schmit <gcschmit@gmail.com>
2018-10-28 20:22:02 -0500

    added for loops starter notes

---
+++ notes/for_loop_notes_starter.py
@@ -0,0 +1,66 @@
+# Intro to for loops notes
+#   counting with the range() function
+ 
+# counting with while loops:
+# CHALLENGE: Print 'Hello, world.' 10 times using a while loop
+
+# FOR LOOP - 
+
+# Print "Hello World" 10 times using a for loop
+input("\nPress enter to begin.")
+
+
+# Display numbers 1-10 using a for loop
+input("\nPress enter to begin.")
+
+
+# Use i for your counter variable (convention used by many programmers,
+#   originating from mathematics)
+#   Display numbers 1-20
+input("\nPress enter to begin.")
+
+
+# Controlling range() in more detail
+#   Count 11-20
+input("\nPress enter to begin.")
+
+
+# Controlling loop increment value
+#   Count by 2's
+input("\nPress enter to begin.")
+
+
+# Practice Challenges:
+#   Increment challenge
+input("\nPress enter to begin.")
+
+
+#   Counting Down Challenge
+input("\nPress enter to begin.")
+
+
+# Application Challenges:
+
+# Bug Collector Program -
+#   A bug collector collects bugs every day for 7 days. Write a program that
+#   keeps a running total of the number of bugs collected during the 7 days.
+#   The loop should ask for the number of bugs collected for each day,
+#   and when the loop is finished, the program should display the total
+#   number of bugs collected.
+
+
+# Calories Burned Program-
+#   Running on a treadmill you burn 3.9 calories per minute.
+#   Write a program that uses a loop to display the number of
+#   calories burned after 10, 15, 20, 25, and 30 minutes.
+
+
+# Advanced Challenge
+
+# Pennies For Pay Program-
+#   Write a program that calculates the amount of money a person would
+#   earn over a period of time if their salary is one penny the first day,
+#   two pennies the second day, and continues to double each day.
+#   The program should ask the user for the number of days worked.
+#   Display a table showing what the salary was for each day,
+#   and then show the total pay at the end of the period.
