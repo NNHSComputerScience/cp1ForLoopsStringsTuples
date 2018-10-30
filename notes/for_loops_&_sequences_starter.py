bb42114d15b11aab5f8b119415167a22d34171d5
Geoff Schmit <gcschmit@gmail.com>
2018-10-29 20:56:08 -0500

    added for loops and sequences starter code

---
+++ notes/for_loops_&_sequences_starter.py
@@ -0,0 +1,62 @@
+# For Loops and Sequences Notes
+# for_loops_&_sequences_starter.py
+
+# SEQUENCE = 
+#   For example: range(5) is an ordered list of numbers: 0,1,2,3,4
+
+# ELEMENT = 
+
+# So far we have used range() to make sequences.
+#   Another type of sequence is a _________,
+#   which is a specific order of letters in a sequence.
+#       For example: 
+
+# ITEREATE = 
+
+# FOR LOOP = 
+
+# So, we can iterate through a string using a for loop just like a range:
+
+
+# CHALLENGE #1: Secret Message - Ask the user for a message and then display
+#       the message with each letter displayed twice.
+#       Ex) message = "great"  output = "ggrreeaatt"
+
+
+# Quick Question: What would display?
+
+
+# ANSWER:
+
+# The len() function:
+#   Used to count the number of elements in a sequence.
+
+
+# Practice: Save your first name to the variable 'name', and then display
+#   how many letters are in your first name.
+
+
+# Practice 2: Now do the same thing for your first and last name.
+#   Be sure to put your first and last in the SAME string.
+#   What did you learn about the len() function?
+
+
+# ANSWER: 
+
+# CHALLENGE #2: Ask the user for their last name.
+#       Tell the user the number of letters in their last name.
+#       Then, determine if the user's name includes the letter "A" or not.
+#       If they do, display "Awesome, you have an A!"
+
+
+# CHALLENGE #3: Password
+#       Ask the user for a password. It must be at least 6 characters long,
+#       and it must contain a '!' or a '$' sign. If they give a valid password
+#       display "Valid Password", otherwise display "Invalid Password".
+
+
+# Adv. CHALLENGE: Can you use a while loop to keep asking for the password
+#       until the user enters it in the correct format?
+
+
+input("\nPress enter to exit.")
