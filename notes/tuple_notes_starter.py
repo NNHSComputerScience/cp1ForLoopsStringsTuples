09c971779e6e8772db25847b4c0cf43efa45f959
Geoff Schmit <gcschmit@gmail.com>
2018-11-08 21:40:35 -0600

    added tuple starter notes

---
+++ notes/tuple_notes_starter.py
@@ -0,0 +1,33 @@
+# Ch. 4: Notes on Tuples
+
+# TUPLE: a type of sequence, like strings, but can contain ANY type of element. 
+
+
+# Creating a Tuple
+
+
+# printing a tuple
+
+
+# CHALLENGE 1: display elements in each tuple with for loop
+
+
+# len()
+
+
+# searching in w/ multiple conditions
+
+
+# CHALLENGE 2: search tuples to find names that are in both using a for loop
+
+        
+# concatenating tuples
+
+
+# concatenating a single element to a tuple
+
+
+# CHALLENGE 3: use a while loop to add new names to a tuple (as many as user wants)
+
+
+input("Press enter to exit.")
