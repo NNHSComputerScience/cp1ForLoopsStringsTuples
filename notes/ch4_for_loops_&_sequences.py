860cec54fc46d5412f3c28ea0d547be2b99d4bc5
Geoff Schmit <gcschmit@gmail.com>
2019-04-17 17:42:25 -0500

    Update ch4_for_loops_&_sequences.py

--- notes/ch4_for_loops_&_sequences.py
+++ notes/ch4_for_loops_&_sequences.py
@@ -1,7 +1,7 @@
 # For Loops and Sequences Notes
 # for_loops_&_sequences_starter.py
 
-# SEQUENCE = an ordered list of element
+# SEQUENCE = an ordered list of elements
 #   For example: range(5) is an ordered list of numbers: 0,1,2,3,4
 
 # ELEMENT = a single item in a sequence
@@ -11,10 +11,10 @@
 #   which is a specific order of letters in a sequence.
 #       For example: "hello" != "olleh"
 
-# ITERATE = to step through a sequence, in order, and one at a time
+# ITERATE = to step through a sequence, in order, one element at a time
 
-# FOR LOOP = iterates through a sequence and executes the body of
-#                           loop for each element in the sequence
+# FOR LOOP = iterates through a sequence and executes the body of the
+#               loop for each element in the sequence
 
 # So, we can iterate through a string using a for loop just like a range:
 for i in "Hello":
@@ -23,7 +23,7 @@ for i in "Hello":
 word = "World"
 for letter in word:
     print(letter)
-
+    
 
 # CHALLENGE #1: Secret Message - Ask the user for a message and then display
 #       the message with each letter displayed twice.
@@ -41,17 +41,19 @@ print()
 
 # ANSWER: HaHa
 
-
 # The len() function:
 #   Used to count the number of elements in a sequence.
-word = "Bananas"
+word = "bananas"
 print(len(word))
 
+if "a" in word:
+    print("there is an 'a' in", word)
 
 # Practice: Save your first name to the variable 'name', and then display
 #   how many letters are in your first name.
 name = "Geoff"
 print(name, "is", len(name), "elements long")
+
 
 # Practice 2: Now do the same thing for your first and last name.
 #   Be sure to put your first and last in the SAME string.
@@ -59,7 +61,10 @@ print(name, "is", len(name), "elements long")
 print("\nGeoff Schmit is", len("Geoff Schmit"), "elements long.")
 print()
 
-# ANSWER: len() counts the spaces! (It counts *every* character in the string)
+
+# ANSWER: len() counts the spaces! It counts *every* character in the string.
+
+
 
 # CHALLENGE #2: Ask the user for their last name.
 #       Tell the user the number of letters in their last name.
@@ -78,14 +83,14 @@ if a_count > 0:
 else:
     print("You don't have an A")
 
-
-# Way #2- another way to use "in"...
-#       Can use it to check to see if an element is in a sequence
-print("\nWay 2:")
+# A more concise alternative using "if" and "in" to check to see if an element
+#	is in a sequence
+print("\nAlternative:")
 if "a" in name.lower():
      print ("Awesome, you have an A!")
 else:
     print("You don't have an A")
+
 
 # CHALLENGE #3: Password
 #       Ask the user for a password. It must be at least 6 characters long,
