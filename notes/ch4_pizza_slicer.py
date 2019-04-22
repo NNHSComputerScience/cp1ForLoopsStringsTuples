7d2928a02e4bcbdb961abaf02a72b3c321645f17
Geoff Schmit <gcschmit@gmail.com>
2019-04-22 13:59:19 -0500

    renamed files; added slicing notes

--- notes/ch4_pizza_slicer.py
+++ notes/ch4_pizza_slicer.py
@@ -7,36 +7,34 @@ word = 'pizza'
 # Note: A slice will always include the first index value specified and
 #       exclude the second index value specified
 
-print(word[3 : 5]) # forwards slice – za
+print(word[3 : 5]) # forward slice; prints: za
 
-print(word[-3 : -6 : -1]) # backwards slice - zip
+print(word[-3 : -6 : -1]) # barkward slice; prints: zip
 
 # slicing shorthand
 #   we can omit values in a slice to represent the start and end of a word
 
-# slice from index 1 to end of the word - izza
+# slice from index 1 to the end of the word: izza
 print(word[1 : ])
 
-# slice from the beginning of the word to the second to last character - pizz
+# slice from the beginning of the word to the 2nd to last character: pizz
 print(word[ : -1])
 
-# slice the entire word - pizza
+# slice the entire word: pizza
 print(word[ : ])
 
-
 # this is useful for making a copy:
-word2 = word[:]
+word2 = word[ : ]
 print(word2)
 
-
-# the step value doesn't have to be +/- 1 - pza
-print(word[0 :: 2])
+# the step value doesn't have to be +/- 1: pza
+print(word[0 : : 2])
 
 # IndexError: string index out of range
 #print(word[5])
 
 # invalid slice returns an empty string
-print("**" + word[5:6] + "**")
+print("**" + word[5 : 6] + "**")
 
 print(
 '''
@@ -52,42 +50,8 @@ print(
 ''')
  
 # CHALLENGE: Write a program that allows the user to slice the word 'pizza'
-#           as many times as they want by getting the beginning and ending 
+#           as many times as they want by inputting the beginning and ending 
 #           index values for the slice.
 word = 'pizza'
 
 print ("Enter the beginning and ending index for your slice of 'pizza'...")
- 
-        
-input('\n\nPress the enter key to exit.')  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
